69d2d0754a26084cc9d9888f65ef2da7ee86047f
Shreyak Malik <shreyakmalik@gmail.com>
2025-10-25 10:38:29 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Task-4
+# Terraform + Docker (IaC)
+
+It provisions a Docker container locally with Terraform.
+
+## Steps
+1. Initialized Terraform with Docker provider.
+2. Pulled `nginx:latest` Docker image.
+3. Created container mapped to port 8080.
+4. Checked container with browser.
+5. Destroyed infrastructure.
+
+## Commands Used
+- terraform init
+- terraform plan
+- terraform apply
+- terraform destroy
---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx_image" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "terraform-nginx"
+  image = docker_image.nginx_image.latest
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
